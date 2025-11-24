1fb2d88f5ef3b3cac84b736f7667e6ce204c8ad3
selvapriya96 <rsselvapriya96@gmail.com>
2025-11-24 18:59:53 +0530

    server changed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import resultRoutes from "./routes/resultRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -8,31 +9,35 @@ import examRoutes from "./routes/examRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js";
 
 dotenv.config();
-
-
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://sp-finalproject-assessment.netlify.app",
-  ],
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://sp-finalproject-assessment.netlify.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/exams", examRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/results", resultRoutes);
 
-
 app.get("/", (req, res) => res.send("API is running..."));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
